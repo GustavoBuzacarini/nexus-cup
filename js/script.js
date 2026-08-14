9b83348eb0492ca33cfb25f7f4e39d5531d5e4b6
Gustavo Buzacarini <gbuzacarini@gmail.com>
2026-08-14 00:40:42 -0300

    feat: Adiciona novas animações para as seções

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 gsap.registerPlugin(ScrollTrigger,ScrollSmoother,SplitText)
 
+// ANIMAÇÃO SCROLL SUAVE
+
 // Para o ScrollSmoother funcionar, é necessário que o conteudo do site esteja dentro de uma div com a classe "smooth-content".
 ScrollSmoother.create({
 	wrapper: "#smooth-wrapper",
@@ -9,3 +11,43 @@ ScrollSmoother.create({
 	effects: true
 });
 
+// ANIMAÇÃO HERO E HEADER
+
+// Animação fade-in do header e do hero
+gsap.from(".header", {
+  opacity: 0,
+  duration: 1.2
+})
+
+gsap.from(".hero", {
+  opacity: 0,
+  duration: 1.2
+})
+
+// Animação cards
+
+gsap.from(".card",{
+	opacity: 0,
+	y: 100,
+	filter: "blur(5px)",
+	stagger: 0.3,
+	scrollTrigger: {
+		trigger: ".card-grid",
+		start: "0% 70%",
+		scrub: true,
+		end: "100% 15%"
+	}
+})
+
+gsap.from(".event-days",{
+	opacity: 0,
+	y: 100,
+	filter: "blur(2px)",
+	stagger: 0.3,
+	scrollTrigger: {
+		trigger: ".event-days",
+		start: "0% 90%",
+		scrub: true,
+		end: "100% 70%",
+	}
+})
